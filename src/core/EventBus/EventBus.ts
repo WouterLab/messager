d46b431d140abe122724d7a28f0dcf4bf08fc6c2
WouterLab <panov135@mail.ru>
2024-01-04 12:50:44 +0300

    WIP block

---
+++ src/core/EventBus/EventBus.ts
@@ -0,0 +1,23 @@
+type EventCallback = (...args: any[]) => void;
+
+export class EventBus {
+  private listeners: { [event: string]: EventCallback[] } = {};
+
+  on(event: string, callback: EventCallback): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  emit(event: string, ...args: any[]): void {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
