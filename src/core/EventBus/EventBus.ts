ab37d8f2f5f9d93ce2e7405b2c01cb0714952480
WouterLab <panov135@mail.ru>
2024-01-07 21:12:55 +0300

    edited eventbus component

--- src/core/EventBus/EventBus.ts
+++ src/core/EventBus/EventBus.ts
@@ -1,7 +1,7 @@
 type EventCallback = (...args: any[]) => void;
 
 export class EventBus {
-  private listeners: { [event: string]: EventCallback[] } = {};
+  private listeners: Record<string, EventCallback[]> = {};
 
   on(event: string, callback: EventCallback): void {
     if (!this.listeners[event]) {
@@ -9,6 +9,16 @@ export class EventBus {
     }
 
     this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: EventCallback): void {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event].filter(
+      (listener) => listener !== callback,
+    );
   }
 
   emit(event: string, ...args: any[]): void {
